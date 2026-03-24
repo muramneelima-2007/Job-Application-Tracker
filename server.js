8781d9618ddaa51551f0ab790d7a9a27f625375a
muramneelima-2007 <237682077+muramneelima-2007@users.noreply.github.com>
2026-03-24 14:10:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+});
